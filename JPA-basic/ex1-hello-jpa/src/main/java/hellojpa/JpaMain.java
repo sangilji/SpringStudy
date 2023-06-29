a3557ac66ddf93da86e443ffb6c0022a2261d6e7
지상일 <gsl0505@naver.com>
2023-06-29 22:25:45 +0900

    feat : init jpa-basic

---
+++ JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        try {
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : resultList) {
+                System.out.println("member.name = " + member.getName());
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+
+    }
+}
